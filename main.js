ddb7c64559c26751a8eeba4b84a2609a74f9fb3b
lukasLssmatpie <148397440+lukasLssmatpie@users.noreply.github.com>
2023-11-15 18:42:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+prediction_1 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+//crie seu modelo e armazene-o na var classifier var
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/816VUKdPV/model.json",modelLoaded);
+//defina a função modelLoaded
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+//defina função check()
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+//defina a função gotResult(error,results)
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("status").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        if(results[0].label == "com"){
+            document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+        }
+        if(results[0].label == "sem"){
+            document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+        }
+    }
+}
